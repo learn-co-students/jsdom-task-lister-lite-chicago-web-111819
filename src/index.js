a4aebdf68924199c92e30cecdb58724b46cd8d51
github username <lluisguardiola.h@gmail.com>
2020-01-07 17:25:28 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const task = document.getElementsByTagName('input');
+	task[1].addEventListener('click', function(event){
+		let tasklist = document.getElementById('tasks')
+		let task = document.createElement('li');
+		task.innerHTML = document.getElementById('new-task-description').value;
+		tasklist.appendChild(task)
+		event.preventDefault();
+	}, false)
 });
